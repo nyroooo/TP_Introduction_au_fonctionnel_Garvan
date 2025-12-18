f5bf83ab2bbf70d373558bda105a7e1d8066535c
garvanboulben <73408584+nyroooo@users.noreply.github.com>
2025-12-18 09:29:22 +0100

    p1-fonctions-pures.ts + réponses aux questions

--- src/p1-fonctions-pures.ts
+++ src/p1-fonctions-pures.ts
@@ -1,11 +1,14 @@
 let compteur = 0;
 
 export function executerP1(): void {
+
     function addition(a: number, b: number): number {
+        // add est prévisible car pour les mêmes paramètres, elle retourne toujours le même résultat et ne modifie aucun état externe.
         return a + b;
     }
 
     function incrementer(): number {
+        // increment n’est pas prévisible car elle modifie une variable globale, son résultat dépend donc des appels précédent.
         compteur++;
         return compteur;
     }
